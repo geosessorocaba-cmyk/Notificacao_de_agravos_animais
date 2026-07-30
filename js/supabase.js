0dc4a0010434ca18fec1c898b3937c3d88b7caa2
Samuel Abreu <36054911+Muelcassiano@users.noreply.github.com>
2026-07-30 16:30:26 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 // js/supabase.js
 
-// 1. URL e Chave Pública do seu projeto Supabase
+// 1. Suas credenciais oficiais do projeto Supabase
 const supabaseUrl = 'https://iwohicnazfxtysetelpz.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3b2hpY25hemZ4dHlzZXRlbHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MDgyNzYsImV4cCI6MjEwMDQ4NDI3Nn0.x8QrBDzCV-qqhkv7zLZYB7FH04NT6CKOcjQOio9TglI';
 
-// 2. Cria a variável global 'supabase' que o app.js vai usar
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+// 2. Cria o cliente do Supabase e o injeta globalmente na janela do navegador (window)
+window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
